8616acd4c041b25ceaaccd0d4531954a2c44818d
User-PC <2356532@mail.ru>
2025-09-17 15:37:36 +0500

    ДЗ 9 готово

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Task 1
+        System.out.println("Задача 1");
+        int[] arr = new int[5];
+        arr[0] = 1000;
+        arr[1] = 1500;
+        arr[2] = 2000;
+        arr[3] = 1000;
+        arr[4] = 1500;
+        int sum = 0;
+        for (int element : arr) {
+            sum = sum + element;
         }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+        System.out.println();
+
+        //Task 2
+        System.out.println("Задача 2");
+        int[] arrayMinAndMax = new int[5];
+        arrayMinAndMax[0] = 12000;
+        arrayMinAndMax[1] = 15000;
+        arrayMinAndMax[2] = 10000;
+        arrayMinAndMax[3] = 11000;
+        arrayMinAndMax[4] = 13000;
+        int min = arrayMinAndMax[0];
+        for (int element : arrayMinAndMax) {
+            if (element < min) {
+                min = element;
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей.");
+        int max = arrayMinAndMax[0];
+        for (int element: arrayMinAndMax) {
+            if (element > max) {
+                max = element;
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю составила " + max + " рублей.");
+        System.out.println();
+
+        //Task 3
+        System.out.println("Задача 3");
+        int[] arrayAverage = new int[5];
+        arrayAverage[0] = 12000;
+        arrayAverage[1] = 13000;
+        arrayAverage[2] = 10000;
+        arrayAverage[3] = 9000;
+        arrayAverage[4] = 15000;
+        float average;
+        float total = 0f;
+        for (int element : arrayAverage) {
+            total = total + element;
+        }
+        average = total / arrayAverage.length;
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
+        System.out.println();
+
+        //Task 4
+        System.out.println("Задача 4");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+        System.out.println();
+
+
     }
 }
